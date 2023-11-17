ef7c41ec5b0dc3650d9fef896cb3bf873f8eb1eb
Icewav3 <coledorman12@gmail.com>
2023-11-16 18:57:26 -0600

    Fixed git ignoring important files
    added prototype concept art for hud
    
    Signed-off-by: Icewav3 <coledorman12@gmail.com>

--- Assets/Scripts/Client/Control.cs
+++ Assets/Scripts/Client/Control.cs
@@ -1,28 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Client;
 using Content;
 using Content.Blocks.MovementBlocks;
-using Ui;
-using Unity.Mathematics;
-using UnityEditor;
-using UnityEngine.UIElements;
+using UnityEngine;
 
 public class Control : MonoBehaviour
 {
     public bool debug = true;
     public float acceleration = 0.01f;
     public float topSpeed = 3;
-    private Vector3 _mousePos;
-    private bool _dead;
-    private int _id;
     private Camera _camera;
-    private Rigidbody2D _player;
-    private bool _editor;
     private GameObject _canvas;
+    private bool _dead;
+    private bool _editor;
+    private int _id;
+    private Vector3 _mousePos;
+    private Rigidbody2D _player;
 
-    
+
     /// <summary>
     /// /Attach this to the parent node of the player, Camera, and Canvas, it manages movement and menu positioning
     /// </summary>
@@ -38,21 +33,30 @@ public class Control : MonoBehaviour
         var testContainer = new ShipContainer("test", new GameObject(), blocks);
         Debug.Log($"Ship Name: {testContainer.shipName}");
         testContainer.UpdateShip();
-        Debug.Log(testContainer.mass);
-        Debug.Log(testContainer.blocks);
+        Debug.Log("testcontainer mass: " + testContainer.mass);
+        Debug.Log("testcontainer name: " + testContainer.blocks);
+        foreach (var VARIABLE in blocks)
+        {
+            Debug.Log("block type: " + VARIABLE.GetType());
+        }
         //END TEST CODE
-        
+
+
         //Load Fuel types
         List<Fuel> fuelList = FuelManager.GetFuelTypes();
 
-       if (debug){foreach (var fuel in fuelList)
-       {
-           Debug.Log($"Fuel Type: {fuel.FuelName}");
-           Debug.Log($"Fuel Color: {fuel.FuelColor}");
-           Debug.Log($"Base Price: {fuel.BasePricePerUnit}");
-       }}
-       //Initialize Player, Camera and Canvas
-       _player = GetComponentInChildren<Rigidbody2D>();
+        if (debug)
+        {
+            foreach (var fuel in fuelList)
+            {
+                Debug.Log($"Fuel Type: {fuel.FuelName}");
+                Debug.Log($"Fuel Color: {fuel.FuelColor}");
+                Debug.Log($"Base Price: {fuel.BasePricePerUnit}");
+            }
+        }
+
+        //Initialize Player, Camera and Canvas
+        _player = GetComponentInChildren<Rigidbody2D>();
         _camera = GetComponentInChildren<Camera>();
         _canvas = gameObject.transform.Find("Canvas").gameObject;
         //Check for nulls
@@ -64,23 +68,27 @@ public class Control : MonoBehaviour
         _id = Time.frameCount;
         _canvas.SetActive(false);
     }
-    
+
     void Update()
     {
         //Build mode
         bool tab = Input.GetKeyDown(KeyCode.Tab);
         _mousePos = _camera.ScreenToWorldPoint(Input.mousePosition); //gets real mouse position
-        
+
         if (debug)
         {
             //print("Current mouse Position " + _mousePos);
         }
+
         if (tab)
         {
-            _editor = !_editor; 
+            _editor = !_editor;
             _canvas.SetActive(_editor); //Toggles UI
             Time.timeScale = _editor ? 0 : 1; //toggles sim
-            if (debug){print("editor state: "+ _editor);}
+            if (debug)
+            {
+                print("editor state: " + _editor);
+            }
         }
         else
         {
@@ -88,59 +96,80 @@ public class Control : MonoBehaviour
             {
                 Player.Create(_id, true);
             }
+
             //onkeypress
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 print(Player.Menu());
             }
+
             //on keyhold
             if (Input.GetKey(KeyCode.W))
             {
                 if (_player.velocity.y < topSpeed)
                 {
-                    _player.velocity += new Vector2(0,acceleration);
+                    _player.velocity += new Vector2(0, acceleration);
                 }
                 else
                 {
-                    _player.velocity = new Vector2(_player.velocity.x,topSpeed);
+                    _player.velocity = new Vector2(_player.velocity.x, topSpeed);
                 }
-                if (debug){print("w");}
+
+                if (debug)
+                {
+                    print("w");
+                }
             }
+
             if (Input.GetKey(KeyCode.S))
             {
                 if (_player.velocity.y > -topSpeed)
                 {
-                    _player.velocity += new Vector2(0,-acceleration);
+                    _player.velocity += new Vector2(0, -acceleration);
                 }
                 else
                 {
-                    _player.velocity = new Vector2(_player.velocity.x,-topSpeed);
+                    _player.velocity = new Vector2(_player.velocity.x, -topSpeed);
                 }
-                if (debug){print("s");}
+
+                if (debug)
+                {
+                    print("s");
+                }
             }
+
             if (Input.GetKey(KeyCode.A))
             {
                 if (_player.velocity.x > -topSpeed)
                 {
-                    _player.velocity += new Vector2(-acceleration,0);
+                    _player.velocity += new Vector2(-acceleration, 0);
                 }
                 else
                 {
                     _player.velocity = new Vector2(-topSpeed, _player.velocity.y);
                 }
-                if (debug){print("a");}
+
+                if (debug)
+                {
+                    print("a");
+                }
             }
+
             if (Input.GetKey(KeyCode.D))
             {
                 if (_player.velocity.x < topSpeed)
                 {
-                    _player.velocity += new Vector2(acceleration,0);
+                    _player.velocity += new Vector2(acceleration, 0);
                 }
                 else
                 {
                     _player.velocity = new Vector2(topSpeed, _player.velocity.y);
                 }
-                if (debug){print("d");}
+
+                if (debug)
+                {
+                    print("d");
+                }
             }
         }
     }
---
+++ Assets/Scripts/Content/ShipContainer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Content.Blocks.MovementBlocks;
+using UnityEngine;
+
+namespace Content
+{
+    public class ShipContainer
+    {
+        public Vector2 angularAcceleration;
+        public Vector2 angularVelocity;
+        public List<Block> blocks;
+        public GameObject core;
+        public Vector2 linearAcceleration;
+        public Vector2 linearVelocity;
+        public float mass;
+        public String shipName;
+
+        public ShipContainer(String shipName, GameObject core, List<Block> blocks)
+        {
+            this.shipName = shipName;
+            this.core = core;
+            this.blocks = blocks;
+        }
+
+        public void UpdateShip()
+        {
+            UpdateMass();
+            UpdateLinearAcceleration();
+        }
+
+        private void UpdateMass()
+        {
+            foreach (Block block in blocks)
+            {
+                mass += block.mass;
+            }
+        }
+
+        private void UpdateLinearAcceleration()
+        {
+            foreach (Block block in blocks)
+            {
+                if (block.GetType() == typeof(ThrusterBlock))
+                {
+                    mass = -1f;
+                }
+            }
+        }
+
+        private void UpdateAngularAcceleration()
+        {
+            //TODO
+        }
+    }
+}
