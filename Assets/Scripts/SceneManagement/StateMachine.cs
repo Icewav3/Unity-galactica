d44fe6417aca35fa0cd26fffd1dcfcdc48247021
Icewav3 <coledorman12@gmail.com>
2023-12-07 21:01:04 -0600

    state machine update
    added eventsystem check and audio listener check

--- Assets/Scripts/SceneManagement/StateMachine.cs
+++ Assets/Scripts/SceneManagement/StateMachine.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace SceneManagement
@@ -6,11 +7,14 @@ namespace SceneManagement
     public class StateMachine : MonoBehaviour
     {
         public static readonly bool
-            DebugMode = true; // Renamed 'Debug' to 'DebugMode' to avoid conflict with UnityEngine.Debug
-
-        private BaseState _currentState;
+            DebugMode = true;
+        // Properties with private setters
+        public EventSystem EventSystem{ get; private set; }
+        public AudioListener AudioListener { get; private set; }
+        //public string CurrentScene { get; private set; }
         public static StateMachine Instance { get; private set; }
-        public string CurrentScene { get; private set; } // Use a property instead of a public field
+        private BaseState _currentState;
+        
 
         private void Awake()
         {
@@ -22,13 +26,17 @@ namespace SceneManagement
             {
                 Instance = this;
                 DontDestroyOnLoad(this.gameObject); // Don't destroy the StateMachine when loading a new scene
+                
+                InitializeEventSystem();
+                InitializeAudioListener();
             }
         }
 
         private void Start()
         {
+            if(DebugMode){Debug.Log("Started in scene: " + SceneManager.GetActiveScene().name);}
             TransitionTo("Game");
-            Debug.Log("Booted scene: " + SceneManager.GetActiveScene().name);
+            if(DebugMode){Debug.Log("Booted to scene: " + SceneManager.GetActiveScene().name);}
         }
 
         private void Update()
@@ -38,16 +46,41 @@ namespace SceneManagement
 
         public static void TransitionTo(string sceneName)
         {
-            Instance.CurrentScene = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(sceneName);
-            SceneManager.UnloadSceneAsync(Instance.CurrentScene);
-            Instance.CurrentScene = SceneManager.GetActiveScene().name;
+            //Instance.CurrentScene = SceneManager.GetActiveScene().name;
+            //Debug.Log(Instance.CurrentScene);
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+            //SceneManager.UnloadSceneAsync(Instance.CurrentScene);
+            //Instance.CurrentScene = SceneManager.GetActiveScene().name;
             UpdateState();
         }
 
         private static void UpdateState()
         {
             Instance._currentState?.Update(); // Null check before updating state
+        }
+        private void InitializeEventSystem()
+        {
+            EventSystem = FindObjectOfType<EventSystem>();
+
+            if (EventSystem == null)
+            {
+                GameObject eventSystemObject = new GameObject("EventSystem");
+                EventSystem = eventSystemObject.AddComponent<EventSystem>();
+                eventSystemObject.AddComponent<StandaloneInputModule>();
+                DontDestroyOnLoad(eventSystemObject);
+            }
+        }
+
+        private void InitializeAudioListener()
+        {
+            AudioListener = FindObjectOfType<AudioListener>();
+
+            if (AudioListener == null)
+            {
+                GameObject audioListenerObject = new GameObject("AudioListener");
+                AudioListener = audioListenerObject.AddComponent<AudioListener>();
+                DontDestroyOnLoad(audioListenerObject);
+            }
         }
     }
 }
