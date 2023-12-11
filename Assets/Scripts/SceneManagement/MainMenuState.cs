720ab636ca07ff3606bf551c7bfb590805b842bd
Icewav3 <coledorman12@gmail.com>
2023-12-11 12:59:21 -0600

    added more concept art
    Signed-off-by: Icewav3 <coledorman12@gmail.com>

--- Assets/Scripts/SceneManagement/MainMenuState.cs
+++ Assets/Scripts/SceneManagement/MainMenuState.cs
@@ -1,7 +1,12 @@
-﻿namespace SceneManagement
+﻿using UnityEngine;
+
+namespace SceneManagement
 {
     public class MainMenuState : BaseState
     {
+        private Camera _camera;
+        private GameObject _canvas;
+
         public override void Update()
         {
             print("MainMenuState");
