d5e608817b17129b9423a121a41c24e934e7790d
Icewav3 <coledorman12@gmail.com>
2024-02-11 19:13:16 -0600

    Implemented KISS principle
    keep it simple stupid

---
+++ Assets/Scripts/Mechanics/AttachPointCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Mechanics
+{
+    public class AttachPointCollider : MonoBehaviour
+    {
+        private BoxCollider2D _collider;
+
+        private void Start()
+        {
+            _collider = GetComponent<BoxCollider2D>();
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButtonDown(0))
+                return;
+
+            if (_collider.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+            {
+                _collider.enabled = false;
+                ConnectBlock();
+            }
+        }
+
+        private void ConnectBlock()
+        {
+            Debug.Log("Connected block at position: " + _collider.transform.position);
+            this.enabled = false; // disable the script
+        }
+    }
+}
